71c81472c0a3e9ccc7e317eca6710264c04d0991
Filip Lukas Paidar <filiplukas.paidar@gmail.com>
2022-11-18 17:19:54 +0100

    Vollständige goToEnd()-Logik fertig

--- interpreter.js
+++ interpreter.js
@@ -1,21 +1,21 @@
 
 let keywords = [
-    'WENN', 'WIEDERHOLE'
+    'WENN', 'WIEDERHOLE', 'SOLANGE'
 ]
 
 let basicCommands = [
-    'SCHRITT', 'LINKS-WENDUNG', 'ABLEGEN', 'AUFHEBEN', 'ENDE'
+    'SCHRITT', 'LINKS-WENDUNG', 'ABLEGEN', 'AUFHEBEN'
 ]
 
 let customCommands = [
 
 ]
 
-interpret(`
-schritt
-wenn
-links-wendung
-nichts
+interpret(`WIEDERHOLE 2-MAL
+WIEDERHOLE 2-MAL
+SCHRITT
+ENDE
+ENDE
 `)
 
 /**
@@ -26,12 +26,36 @@ function interpret(code, name=0) {
 
     let lines = code.toUpperCase().split('\n')
 
-    for (let line of lines) {
+    for (let i = 0; i < lines.length; i++) {
 
-        line = line.trim()
+        line = lines[i].trim()
 
         if (keywords.some(v => line.includes(v))) {
-            console.log(`keyword command: ${line}`)
+            if (/^WENN (IST|NICHT) [A-Z]+$/.test(line)) {
+                // if-statement
+                let bedingung = { type: line.split(' ')[1], name: line.split(' ')[2]}
+                let result = getToEnd(lines, i+1)
+                i = result.i;
+                if (check(bedingung)) interpret(result.string)
+            } else if (/^WIEDERHOLE SOLANGE (IST|NICHT) [A-Z]+$/.test(line)) {
+                // while-loop
+                let bedingung = { type: line.split(' ')[2], name: line.split(' ')[3]}
+                let result = getToEnd(lines, i+1)
+                i = result.i;
+                while (check(bedingung)) interpret(result.string)
+            } else if (/^WIEDERHOLE [0-9]+\-MAL$/.test(line)) {
+                // for-loop
+                let n = parseInt(line.split(' ')[1].split('-')[0])
+                let result = getToEnd(lines, i+1)
+                i = result.i;
+                for (let j = 0; j < n; j++) {
+                    interpret(result.string)
+                }
+            }
+            else {
+                console.error(`UNVOLLSTÄNDIGE ODER FALSCHE VERWENDUNG VON KEYWORDS: ${line}`)
+            }
+            
         } else if (basicCommands.includes(line)) {
             console.log(`basic command: ${line}`)
         } else if (customCommands.includes(line)) {
@@ -39,5 +63,26 @@ function interpret(code, name=0) {
         }
 
     }
+}
 
+function getToEnd(lines, i) {
+    let loops = 1;
+    let string = "";
+    for (i; loops !== 0; i++) {
+        let line = lines[i].trim()
+        if (line.match(/(^WENN (IST|NICHT) [A-Z]+$)|(^WIEDERHOLE SOLANGE (IST|NICHT) [A-Z]+$)|(^WIEDERHOLE [0-9]+\-MAL$)/)) {
+            loops++;
+            string += `${line}\n`
+        } else if (line == 'ENDE') {
+            loops--;
+            if (loops !== 0) string += `${line}\n`
+        } else {
+            string += `${line}\n`
+        }
+    }
+    return { i, string }
+}
+
+function check(bedingung) {
+    return true;
 }
