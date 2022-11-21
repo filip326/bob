f83708c1bf808379cde44e0029e5a01fe1fcc8fe
Barsch2006 <cchr98756@gmail.com>
2022-11-21 18:09:35 +0100

    grossschreibung finished

--- syntaxHelp.js
+++ syntaxHelp.js
@@ -5,15 +5,27 @@ textarea.value = localStorage.getItem('textarea') || ""
 
 let savingCache = cacher(1000)
 
-textarea.addEventListener('input', () => {
+textarea.addEventListener('keydown', (key) => {
+    if (key.key == 'Enter') {
+        grossschreiben(textarea.value)
+        savingCache(() => {
+            localStorage.setItem('textarea', textarea.value)
 
-    savingCache(() => {
-        localStorage.setItem('textarea', textarea.value)
-
-        log(`&orCode gespeichert`)
-    })
-
+            log(`&orCode gespeichert`)
+        })
+    } else {
+        console.log(key.key)
+    }
 })
+
+function grossschreiben(t) {
+    textarea.value = null;
+    var text = t.split('\n')
+    for (line in text) {
+        text[line] = text[line].toUpperCase() + '\n';
+        textarea.value += text[line];
+    }
+}
 
 function cacher(ms) {
 
