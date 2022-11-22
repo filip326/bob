ce6fa9dd4fb2600a33bb23b32bf0be1bc2d79d0d
Filip Lukas Paidar <filiplukas.paidar@gmail.com>
2022-11-22 19:28:03 +0100

    Syntax highlighting

--- syntaxHelp.js
+++ syntaxHelp.js
@@ -3,12 +3,11 @@ let textarea = document.querySelector('.editor textarea')
 /** @type {HTMLDivElement} */
 let syntaxHighlighter = document.querySelector('#editor-overlay')
 
-textarea.value = localStorage.getItem('textarea') || ""
 
 let savingCache = cacher(1000)
 
 textarea.addEventListener('keydown', (key) => {
-    if (key.key == 'Enter') {
+    if (key.code == 'Space' || key.code == 'Enter') {
         grossschreiben(textarea.value)
         savingCache(() => {
             localStorage.setItem('textarea', textarea.value)
@@ -18,59 +17,57 @@ textarea.addEventListener('keydown', (key) => {
     }
 })
 
-textarea.addEventListener('input', (e) => {
+function syntaxHighlighting() {
     let lines = textarea.value.split('\n');
-
     syntaxHighlighter.innerHTML = "";
+
+    let allCommands = [
+        ...(textarea.value.match(/LERNE [A-Z0-9\-]+/g)?.map((v) => v.split(' ')[1]) || []),
+        'WIEDERHOLE', 'WENN', 'SOLANGE', 'LERNE', 'ENDE, SONST', 'ENDE', '-MAL',
+        'SCHRITT', 'LINKS-WENDUNG', 'PLATZIEREN', 'AUFHEBEN',
+        'IST', 'NICHT'
+    ]
 
     for (let line of lines) {
 
         let html = line;
 
         html.replace(/</, `&lt;`)
-        html.replace(/>/, `&gt;`)
+        html.replace(/>/, `&gt;`);
 
-        html.replace(/\/\/.+$/gm, `<span class="comment">$&</span>`)
-        html.replace(/WIEDERHOLE|WENN|SOLANGE|LERNE|ENDE|-MAL/gm, `<span class="keyword">$&</span>`)
-        html.replace(/SCHRITT|LINKS-WENDUNG|PLATZIEREN|AUFHEBEN/gm, `<span class="basicCommands">$&</span>`)
-        html.replace(/IST|NICHT/gm, `<span class="logic-operator">$&</span>`)
-        html.replace(/[0-9]+/gm, `<span class="number">$&</span>`)
+
+        html = html.replace(new RegExp(allCommands.join('|'), 'g'), '<span class="not-wrong">$&</span>')
+        html = html.replace(/\/\/.+$/gm, `<span class="comment">$&</span>`)
+        html = html.replace(/WIEDERHOLE|WENN|SOLANGE|LERNE|ENDE, SONST|ENDE|-MAL/gm, `<span class="keyword">$&</span>`)
+        html = html.replace(/SCHRITT|LINKS-WENDUNG|PLATZIEREN|AUFHEBEN/gm, `<span class="basicCommand">$&</span>`)
+        html = html.replace(/IST|NICHT/gm, `<span class="logic-operator">$&</span>`)
+        html = html.replace(/[0-9]+/gm, `<span class="number">$&</span>`)
 
         let div = document.createElement('div')
 
+        if (html.trim() == "") { html = `<span class="invisible">__</span>` }
         div.innerHTML = html;
+
 
         syntaxHighlighter.appendChild(div)
 
     }
+}
 
-    /*
-
-    string.replace(/</, `&lt;`)
-    string.replace(/>/, `&gt;`)
-
-    string.replace(/\/\/.+$/gm, `<span class="comment">$&</span>`)
-    string.replace(/WIEDERHOLE|WENN|SOLANGE|LERNE|ENDE|-MAL/gm, `<span class="keyword">$&</span>`)
-    string.replace(/SCHRITT|LINKS-WENDUNG|PLATZIEREN|AUFHEBEN/gm, `<span class="basicCommands">$&</span>`)
-    string.replace(/IST|NICHT/gm, `<span class="logic-operator">$&</span>`)
-    string.replace(/[0-9]+/gm, `<span class="number">$&</span>`)
-    string.replace(/\n/gm, `<br />`)
-
-    console.log(/SCHRITT|LINKS-WENDUNG|PLATZIEREN|AUFHEBEN/gm.test(string))
-
-    syntaxHighlighter.innerHTML = string;
-    */
-})
+textarea.addEventListener('input', syntaxHighlighting)
 
 function grossschreiben(t) {
     let cursorPos1 = textarea.selectionStart;
     let cursorPos2 = textarea.selectionEnd;
+    let string = ""
     textarea.value = null;
     var text = t.split('\n')
     for (line in text) {
         text[line] = text[line].toUpperCase() + '\n';
-        textarea.value += text[line];
+        string += text[line];
     }
+    string = string.trimEnd()
+    textarea.value = string;
     textarea.setSelectionRange(cursorPos1, cursorPos2)
 }
 
@@ -86,4 +83,7 @@ function cacher(ms) {
     }
 
     return make;
-} 
+}
+textarea.value = localStorage.getItem('textarea') || ""
+
+syntaxHighlighting()
